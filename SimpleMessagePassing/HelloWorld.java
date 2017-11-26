993deb8da26f6d1bc8fa33ac0964df405c1f9cc4
Sorrachai Yingchareonthawornchai <sorrachai.cp@gmail.com>
2017-11-26 10:55:19 -0500

    Import Jgroup

--- SimpleMessagePassing/HelloWorld.java
+++ SimpleMessagePassing/HelloWorld.java
@@ -1,9 +1,10 @@
+import org.jgroups.JChannel;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Worl");
+		System.out.println("Hello World");
 	}
 
 }
